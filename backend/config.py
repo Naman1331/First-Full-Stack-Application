e4455dfc9780cd961cf46922ee41d6d5988ecacd
Naman1331 <naman.gandhi@skilljourney.in>
2024-07-01 14:39:11 +0530

    Updated backend

--- backend/config.py
+++ backend/config.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-app=Flask(__main__)
+app=Flask(__name__)
 CORS(app)
 
-app.config["SQLAlchemy_DATABASE_URI"]="sqlite:///mydatabase.db"
-app.config["SQLAlchemy_TRACK_MODIFICATIONS"]=False
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///mydatabase.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 
 db=SQLAlchemy(app)
